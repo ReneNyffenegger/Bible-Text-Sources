70ec39a408dcfa597f5d886120fa0f782db27090
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2015-10-26 00:09:06 +0100

    Remove starting whitespace

--- www.byztxt.com/scripts/010_oneLinePerVerse.pl
+++ www.byztxt.com/scripts/010_oneLinePerVerse.pl
@@ -114,7 +114,7 @@ sub do_book { # {{{
   $file_text =~ s/^ *0*(\d+):0*(\d+) */QQQ$book_abbr-$1-$2|/gm;
 
   $file_text =~ s/\n/ /g;
-  $file_text =~ s/QQQ//;
+  $file_text =~ s/ *QQQ//;
   $file_text =~ s/QQQ/\n/g;
   $file_text .= "\n";
   $file_text =~ s/ +/ /g;
