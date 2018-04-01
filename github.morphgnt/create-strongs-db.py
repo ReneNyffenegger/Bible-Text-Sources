d8c16d597ca8bc25a10e282a91ab7de1a28cba8e
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-04-01 15:29:16 +0200

    create sqlite db

--- github.morphgnt/create-strongs-db.py
+++ github.morphgnt/create-strongs-db.py
@@ -1,14 +1,21 @@
-# from xml.dom import minidom
-
-# dom = minidom.parse('strongs-dictionary-xml/strongsgreek.xml')
-
 import xml.etree.ElementTree as ET
 
-# for entry in dom.entries
-#     print(entry)
+import os.path
+import sqlite3
 
 tree = ET.parse('strongs-dictionary-xml/strongsgreek.xml')
 root = tree.getroot()
+
+db_name = 'strongs.db'
+if os.path.isfile(db_name):
+   os.remove(db_name)
+
+db  = sqlite3.connect(db_name)
+cur = db.cursor()
+cur.execute("""create table strongs (
+  nr   integer not null primary key,
+  word text    not null
+)""")
 
 for entry in root.findall('entries/entry'):
 
@@ -16,5 +23,6 @@ for entry in root.findall('entries/entry'):
     if greek is not None:
        strongs_no = int(entry.findtext('./strongs'))
        greek_unicode = greek.attrib['unicode']
-       print(str(strongs_no) + ': ' + greek_unicode)
+       cur.execute('insert into strongs(nr, word) values (?, ?)', (strongs_no, greek_unicode))
 
+cur.execute('commit')
