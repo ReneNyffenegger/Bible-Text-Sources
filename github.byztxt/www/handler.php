4ebb52b0cf0a0d1baaa2b911e9ee15848e48026e
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-04-16 11:13:13 +0200

    Schema nachziehen

--- github.byztxt/www/handler.php
+++ github.byztxt/www/handler.php
@@ -114,7 +114,7 @@ function index($db) { #_{
 
 function strongs_alle($db) { #_{
 
-  $res = db_prep_exec_fetchall($db, 'select nr, word from strongs_greek', array());
+  $res = db_prep_exec_fetchall($db, 'select nr, word, word_de from strongs order by nr', array());
 
   if (! $res) {
     print("Well...<br>");
@@ -123,7 +123,7 @@ function strongs_alle($db) { #_{
 
   print("<table>");
   foreach ($res as $row) {
-    printf("<tr><td><a href='Strongs-%d'>%s</a></td></tr>", $row['nr'], $row['word']);
+    printf("<tr><td><a href='Strongs-%s'>%s</a></td><td>%s</td></tr>", $row['nr'], $row['word'], $row['word_de']);
   }
   print("</table>");
 
@@ -132,12 +132,15 @@ function strongs_alle($db) { #_{
 function show_verses_with_strongs($db, $nr) { #_{
 
   $db_strongs = db_connect('strongs.db');
-  $row_strongs = db_prep_exec_fetchrow($db_strongs, 'select word, text_de from strongs where nr = ?', array($nr));
+  $row_strongs = db_prep_exec_fetchrow($db_strongs, 'select word, word_de, strongs_en, strongs_de from strongs where nr = ?', array($nr));
 
   print("<h1>Strongs $nr (" . $row_strongs['word'] .")</h1>");
 
-  $text_de = $row_strongs['text_de']; # Google Übersetzung
-  $text_de = preg_replace_callback('/(G)(\d+)/',
+  $word_de = $row_strongs['word_de'];
+  $strongs_en = $row_strongs['strongs_en'];
+
+  $strongs_de = $row_strongs['strongs_de']; # Google Übersetzung
+  $strongs_de = preg_replace_callback('/(G)(\d+)/',
     function($m) use ($db_strongs) {
 #     $strongs_nr_ = $m[1];
       $strongs_nr_ = $m[1] . str_pad($m[2], 4, '0', STR_PAD_LEFT);
@@ -146,12 +149,16 @@ function show_verses_with_strongs($db, $nr) { #_{
        $row_strongs_ = db_prep_exec_fetchrow($db_strongs, 'select word from strongs where nr = ?', array($strongs_nr_));
        return "<a href='Strongs-$strongs_nr_'>" . $row_strongs_['word'] . "</a>";
     },
-    $text_de
+    $strongs_de
     );
 
+  print "<h2>$word_de</h2>";
+
+  print "Strong's Eintrag:";
+  print "<pre style='background-color:#c9ffaf; border:1px solid black'><code>" . $strongs_en . "</code></pre>";
+
   print "Google-Übersetzung vom Strong's-Eintrag:";
-# print "<pre style='background-color:#c9faff; border:1px solid black'><code>" . $row_strongs['text_de'] . "</code></pre>";
-  print "<pre style='background-color:#c9faff; border:1px solid black'><code>" . $text_de . "</code></pre>";
+  print "<pre style='background-color:#c9faff; border:1px solid black'><code>" . $strongs_de . "</code></pre>";
 
 
   $res_1 = db_prep_exec_fetchall($db, 'select distinct v_id, b, c, v from word_v where strongs = ?', array($nr));
@@ -183,34 +190,8 @@ function show_verses_with_strongs($db, $nr) { #_{
 
   }
 
-# $res = db_prep_exec_fetchall($db, "with v as (
-#     select distinct b, c, v from word_v where strongs = $nr
-#   )
-#   select
-#     w.strongs, w.b, w.c, w.v, w.word
-#   from
-#     word_v w join v on w.b = v.b and w.c = v.c and w.v = v.v
-#   order by
-#     w.b, w.no
-#  ", array()
-# ); 
+  print("<hr><a href='Strongs'>Alle Strongs Nummern</a>");
 
-# $last_b = '?';
-# $last_c = 0;
-# $last_v = 0;
-
-# foreach ($res as $row) {
-#   if ($last_b != $res['b'] or $last_c != $res['c'] or $last_v != $res['v']) {
-
-#     printf("<p><a href='%s-%d'>%s-%d-%d</a>: ", $res['b'], $res['c'], $res['b'], $res['c'], $res['v']);
-
-#     $last_b = $res['b'];
-#     $last_c = $res['c'];
-#     $last_v = $res['v'];
-#   }
-
-#   printf("<a href='Strongs-%d'>%s</a> ", $res['strongs'], to_greek_letters($res['word']));
-# }
 
 } #_}
 
