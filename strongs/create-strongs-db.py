841e0d02f900c337fefea8d5cb6ccbf629f10de8
René Nyffenegger <rene.nyffenegger@adp-gmbh.c>
2018-04-17 17:16:53 +0200

    update's

--- strongs/create-strongs-db.py
+++ strongs/create-strongs-db.py
@@ -31,6 +31,12 @@ cur.execute("""create table strongs_see (
   unique (nr_1, nr_2)
 )""")
 
+cur.execute("""create table strongs_noun_adj_verb (
+  noun       text references strongs,
+  adj        text references strongs,
+  verb       text references strongs
+)""")
+
 
 strongs_xx = {}
 
@@ -76,7 +82,7 @@ def read_strong(lang, nr_expected):
       if not strongs_xx[lang]['line']:
     #    cur_strongs_en_nr = next_strongs_de_nr
          return ret
-  
+
 #     m = re.search('^(\d+)@', line_strongs_en)
       m = re.search('^(\d+) *@', strongs_xx[lang]['line'])
       if m:
@@ -120,34 +126,71 @@ def extract_gerhard_kautz_I(t):
     r = re.sub(r'^ *: *'      , '', r)
     return r
 
+def update_strongs():
+    cur.execute('update strongs set word_de = ? where nr = ?', ('adramytisch'       , 'G0098'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('sondern'           , 'G0235'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('Einmischender'     , 'G0244'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('-'                 , 'G0302')) # Modalpartikel welche meist unübersetzbar ist; bezeichnet Handlung
+    cur.execute('update strongs set word_de = ? where nr = ?', ('sich verabschieden', 'G0685'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('Fluchen'           , 'G0685'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('unsichtbar'        , 'G0855')) # ..er ward entschwunden, weg von ihnen...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('entfernen'         , 'G0868')) # tr. (im , Impf., Fut. und Aor1): abtrünnig machen)
+    cur.execute('update strongs set word_de = ? where nr = ?', ('an der andern Seite vorübergehen', 'G0942')) # an der gegenüberliegenden (Straßenseite) vorübergehen
+    cur.execute('update strongs set word_de = ? where nr = ?', ('bezaubern'         , 'G0940')) # jmd. verhexen
+    cur.execute('update strongs set word_de = ? where nr = ?', ('denn'              , 'G1063'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('gottergeben'       , 'G1174')) # als Komp. (für Superl.): mehr Götter fürchtend...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('-'                 , 'G1473')) # ich (mein, mir, mich); betont: i c h (m e i n, m i r, m i c h)
+    cur.execute('update strongs set word_de = ? where nr = ?', ('ähneln'            , 'G1503')) # Perfekt mit Präsensbedeutung: ist ähnlich...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('in'                , 'G1519')) # örtl.: hinein...in
+    cur.execute('update strongs set word_de = ? where nr = ?', ('aus'               , 'G1537')) # aus...(heraus)
+    cur.execute('update strongs set word_de = ? where nr = ?', ('dort'              , 'G1563')) # als Antwort auf Frage: Wo?: dort
+    cur.execute('update strongs set word_de = ? where nr = ?', ('rum-huren'         , 'G1608')) # Aor.fem.: außergewöhnlich gehurt-Habende
+    cur.execute('update strongs set word_de = ? where nr = ?', ('in'                , 'G1722')) # örtl.: auf
+    cur.execute('update strongs set word_de = ? where nr = ?', ('auf'               , 'G1909')) # örtl.: auf
+    cur.execute('update strongs set word_de = ? where nr = ?', ('haben'             , 'G2192')) # Akt. haben
+    cur.execute('update strongs set word_de = ? where nr = ?', ('bis'               , 'G2193')) # als Konj. - solange bis...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('oder'              , 'G2228')) # trennend: oder
+    cur.execute('update strongs set word_de = ? where nr = ?', ('damit'             , 'G2443')) # damit...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('stellen'           , 'G2476')) # tr.(, Impf., Aor.1 Akt. und Fut.Akt.): stellen
+    cur.execute('update strongs set word_de = ? where nr = ?', ('und'               , 'G2532'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('herab'             , 'G2596')) # räuml.: herab
+    cur.execute('update strongs set word_de = ? where nr = ?', ('erwarten'          , 'G3195'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('inmitten'          , 'G3326')) # örtl. inmitten
+    cur.execute('update strongs set word_de = ? where nr = ?', ('kurzsichtig'       , 'G3467'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('-'                 , 'G3588'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('damit'             , 'G3704'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('refl. pr.'         , 'G3739')) # welche (-er, -en, -es, usw.)
+    cur.execute('update strongs set word_de = ? where nr = ?', ('daß'               , 'G3754')) # daß...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('nicht'             , 'G3756')) # nein
+    cur.execute('update strongs set word_de = ? where nr = ?', ('niemand'           , 'G3762')) # nicht ein (-er, -e, -en, -es, usw.)
+    cur.execute('update strongs set word_de = ? where nr = ?', ('-'                 , 'G3801')) # der "Seiende" und der "Er war" und der "Kommende"
+    cur.execute('update strongs set word_de = ? where nr = ?', ('zurück'            , 'G3825')) # örtl.: zurück
+    cur.execute('update strongs set word_de = ? where nr = ?', ('beistehen'         , 'G3936')) # im Akt. , Ipf., Aor.1, und Fut.): darstellen
+    cur.execute('update strongs set word_de = ? where nr = ?', ('alle'              , 'G3956'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('sehr'              , 'G4007')) # im NT immer an ein anderes Wort angehängt um diesem eine positive<br>Betonung zu geben - hervorhebend oder verschärfend: ... wirklich; ... anders;<br>durchaus; eben.
+    cur.execute('update strongs set word_de = ? where nr = ?', ('umhergerissen'     , 'G4049')) # Ind.Impf....war ständig hin- und hergerissen
+    cur.execute('update strongs set word_de = ? where nr = ?', ('Ansehen-Betrachtender', 'G4381')) # der auf Ansehen (Person Rücksicht) Nehmende
+    cur.execute('update strongs set word_de = ? where nr = ?', ('verfault'          , 'G4595')) # intr. Ind.Pf.Akt. im pass. Sinn: verfault
+    cur.execute('update strongs set word_de = ? where nr = ?', ('anwerben'          , 'G4758')) # Aor.: zum Kriegsdienst angeworben Habende
+    cur.execute('update strongs set word_de = ? where nr = ?', ('Band'              , 'G4886')) # (zusammenhaltende) Band
+    cur.execute('update strongs set word_de = ? where nr = ?', ('sehr'              , 'G4970')) # in Verbindung mit einem Adj. oder einem Zeitwort: sehr ...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('Heil'              , 'G4992')) # als Adj. ohne Art., prädikativ: Errettung bringend (für)...
+    cur.execute('update strongs set word_de = ? where nr = ?', ('jemand'            , 'G5100')) # irgendeiner, -e, -es
+    cur.execute('update strongs set word_de = ? where nr = ?', ('wer'               , 'G5101')) # wer (was, welcher)
+    cur.execute('update strongs set word_de = ? where nr = ?', ('sicher'            , 'G5104')) # wird nur in Verbindung mit anderen Partikeln gebraucht: gewiß
+    cur.execute('update strongs set word_de = ? where nr = ?', ('Witwe'             , 'G5503'))
+    cur.execute('update strongs set word_de = ? where nr = ?', ('Zwerchfell'        , 'G5424')) # Verstand(esregungen)
+
+def noun_adj_verb():
+    cur.execute('insert into strongs_noun_adj_verb values (?, ?, ?)', ('G2549', 'G2556', None)) # κακία - κακός
+
+
 for entry in root.findall('entries/entry'):
 
     strongs_de = ''
     strongs_en = ''
-#q    while True:
-#q    
-#q        l = strongs_de_f.readline()
-#q        if not l:
-#q           cur_strongs_de_nr = next_strongs_de_nr
-#q           break
-#q    
-#q        m = re.search('^(\d+) *@', l)
-#q        if m:
-#q           next_strongs_de_nr = int(m[1])
-#q#          print('next_strongs_de_nr: {:d}'.format(next_strongs_de_nr))
-#q#          if cur_strongs_de_nr != next_strongs_de_nr - 2:
-#q#             print('! next_strongs_de_nr: {:d}, cur_strongs_de_nr: {:d}'.format(next_strongs_de_nr, cur_strongs_de_nr))
-#q    
-#q#          cur_strongs_de_nr = next_strongs_de_nr -1
-#q           break
-#q        else:
-#q
-#q           strongs_de += l
 
 
-#   print ('... cur_strongs_de_nr: {:d}'.format(cur_strongs_de_nr))
-#   print (strongs_de)
-#   sys.exit(1)
 
     greek = entry.find('./greek')
     if greek is not None:
@@ -215,16 +258,38 @@ for entry in root.findall('entries/entry'):
 #      for see in entry.findall("./see/[@language='HEBREW']"):
 #          print('x: ' + see.attrib['strongs'])
 
+def see_also(nr_1, nr_2):
+    try:
+      cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', (nr_1, nr_2))
+    except sqlite3.IntegrityError as e:
+      print('! {:s} nr_1 = {:s}, nr_2 = {:s}'.format(str(e), nr_1, nr_2))
+    try:
+      cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', (nr_2, nr_1))
+    except sqlite3.IntegrityError as e:
+      print('! {:s} nr_1 = {:s}, nr_2 = {:s}'.format(str(e), nr_1, nr_2))
 
 # TQ84's entries:
 #
-# δῶρον <--> χάρις
-cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G5485', 'G1435'))
-cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G1435', 'G5485')) 
+see_also('G5485', 'G1435') # δῶρον <--> χάρις
+# cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G5485', 'G1435'))
+# cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G1435', 'G5485'))
+
+see_also('G1722', 'G1519') # ἐν <--> εἰς
+# cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G1722', 'G1519'))
+# cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G1519', 'G1722'))
+
+see_also('G0894', 'G4088') # ἄψινθος <--> ...  Wermut / Bitterkeit
+# cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G0894', 'G4088'))
+# cur.execute('insert into strongs_see(nr_1, nr_2) values (?, ?)', ('G4088', 'G0894'))
+
+see_also('G2549', 'G4189') #  κακία <-->  πονηρία   ( 1. Kor 5:8 )
 
 #
 #  select count(*), nr, nr_greek from strongs_greek_see group by nr, nr_greek having count(*) > 1;
 #
 # cur.execute('delete from strongs_greek_see where rowid in (select max(rowid) from strongs_greek_see group by nr, nr_greek having count(*) > 1)')
+
+update_strongs()
+noun_adj_verb()
 
 cur.execute('commit')
